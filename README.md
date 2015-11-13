841e23814651ffb53761b8ab97d3d8b0ce48dd5e
Geert Barentsen <geert@barentsen.be>
2015-11-13 09:48:48 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# earth-state-vector
-Prints the heliocentric state vector of the earth using SpiceyPy (SPICE)
+# earth-state-vector 
+***Prints the heliocentric state vector of the earth using SpiceyPy (SPICE).***
+
+A little tool intended to demonstrate the use of SPICE, through the [SpiceyPy Python wrapper](http://spiceypy.readthedocs.org), to print the state vector of the earth (i.e. position and velocity) in heliocentric ecliptic (J2000) coordinates.
+
+### Example use
+```
+$ python earth-state-vector.py 2015-11-13T09:43:00
+
+Input time = 2015-11-13T09:43:00
+
+# Position of the Earth in the heliocentric ecliptic (J2000) frame
+X = 94234331.6256016 km
+Y = 114193407.5443151 km
+Z = -4953.757979899645 km
+
+# Velocity of the Earth in the same frame
+dX = -23.4721139580071 km/s
+dY = 18.852699520611225 km/s
+dZ = -0.0009050242759034433 km/s
+
+# Orbital speed [sqrt(dX^2 + dY^2 + dZ^2)]
+Speed = 30.10588669498368 km/s
+```
+
+### Installation
+
+[SpiceyPy](http://spiceypy.readthedocs.org) must be installed.
+For example:
+```
+$ git clone git@github.com:AndrewAnnex/SpiceyPy.git
+$ cd SpiceyPy
+$ python setup.py install
+```
---
+++ earth-state-vector.py
@@ -0,0 +1,42 @@
+"""Prints the heliocentric state vector of the earth using SpiceyPy (SPICE).
+"""
+from __future__ import print_function  # Needed for Python 2 compatibility
+import sys
+import spiceypy.wrapper as sp
+
+# These data files were obtained from
+# http://naif.jpl.nasa.gov/pub/naif/generic_kernels
+LEAP_SECOND_FILE = "data/naif0011.tls"
+EPHEMERIDES_FILE = "data/de432s.bsp"
+
+
+if __name__ == "__main__":
+    # Script expects first argument to be a UTC timestamp
+    if len(sys.argv) > 1:
+        input_utc = sys.argv[1]
+    else:
+        input_utc = "2000-01-01T00:00:00"  # default for testing
+    # Load the necessary data files
+    sp.furnsh([LEAP_SECOND_FILE, EPHEMERIDES_FILE])
+    # Convert UTC to ephemeris time
+    epoch = sp.utc2et(input_utc)
+    # State (position and velocity in cartesian coordinates)
+    # of EARTH as seen from SUN in the ecliptic J2000 coordinate frame.
+    state, lt, = sp.spkezr("EARTH", epoch, "ECLIPJ2000", "NONE", "SUN")
+    # Show the output
+    print("Input time = {}".format(input_utc))
+    print("")
+    print("# Position of the Earth in the heliocentric ecliptic (J2000) frame")
+    print("X = {} km".format(state[0]))
+    print("Y = {} km".format(state[1]))
+    print("Z = {} km".format(state[2]))
+    print("")
+    print("# Velocity of the Earth in the same frame")
+    print("dX = {} km/s".format(state[3]))
+    print("dY = {} km/s".format(state[4]))
+    print("dZ = {} km/s".format(state[5]))
+    # As a sanity check, print the speed
+    speed = (state[3]**2 + state[4]**2 + state[5]**2)**.5
+    print("")
+    print("# Orbital speed [sqrt(dX^2 + dY^2 + dZ^2)]")
+    print("Speed = {} km/s".format(speed))
